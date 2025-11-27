4d6fd9215d3690d574940f79c27a04ca8d67a1cf
pranavkulkarni7-del <pranavkulkarni834@gmail.com>
2025-11-27 17:25:43 +0530

    Fix indentation in journal1.py

--- journal1.py
+++ journal1.py
@@ -2,12 +2,12 @@ import sys
 
 if len(sys.argv) == 4:
    principal = sys.argv[1]
-    rate = sys.argv[2]
-    time = sys.argv[3]
+   rate = sys.argv[2]
+   time = sys.argv[3]
 else:
-    principal = 900
-    rate = 0.1
-    time = 5
+   principal = 900   
+   rate = 0.1
+   time = 5
 
 interest = (principal * rate * time) / 100
 
