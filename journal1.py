5b948223fce1bf6ed2bb9595c19ad61b941ac95c
pranavkulkarni7-del <pranavkulkarni834@gmail.com>
2025-11-27 17:31:24 +0530

    Update journal1.py

--- journal1.py
+++ journal1.py
@@ -9,10 +9,10 @@ else:
     rate = 0.1
     time = 5
 
-interest = (principal * rate * time) / 100
+interest=(principal*rate*time)/100
 
 print("\n--- Simple Interest Calculator ---")
-print(f"Principal Amount: ₹{principal}")
-print(f"Rate of Interest: {rate}%")
-print(f"Time Period: {time} years")
-print(f"Simple Interest: ₹{interest:.2f}")
+print("Principal Amount: ₹",principal)
+print("Rate of Interest: ",rate})
+print("Time Period:",time)
+print("Simple Interest: ₹",interest)
